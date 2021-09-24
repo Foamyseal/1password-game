ef92077805840a12b0b1bcb4561b35f89071de21
harsimrankanwar <harsimran.kanwar@yahoo.ca>
2021-09-24 17:39:40 -0400

    added header component

---
+++ app/javascript/one_password_game/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Typography from '@mui/material/Typography';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+
+function Header() {
+    return (
+        <AppBar position="static" style={{ background: '#FFF4DE' }}>
+            <Toolbar variant="dense">
+                <IconButton edge="end" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <InfoOutlinedIcon color="primary" />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}/>
+                <IconButton size="large">
+                    <StorefrontOutlinedIcon color="primary" />
+                </IconButton>
+                <IconButton size="large">
+                    <InfoOutlinedIcon color="primary" />
+                </IconButton>
+
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export { Header };
