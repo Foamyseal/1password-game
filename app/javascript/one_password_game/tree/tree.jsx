a1732533e5abc3748f5528e37fa897fa6cd564b8
Michael-Xie <xiemicha@gmail.com>
2021-09-24 18:00:22 -0400

    Add tree

---
+++ app/javascript/one_password_game/tree/tree.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// Based on the stem generator in Flash Math Creativity
+
+
+
+
+
+
+const UpdatingTree = (props) => {
+    return (
+      <div>
+        <p>"beans"</p>
+      </div>
+    );
+  };
+  
+
+
+
